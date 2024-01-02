609c17e245ddaacff777d17346c897195eea6b77
Esdras Mendes <esdrasmendes0@outlook.com>
2024-01-02 19:39:54 -0300

    Update Child process

--- videos/cp.js
+++ videos/cp.js
@@ -1,2 +1,14 @@
-const { spawn } = require('child_process')
-const ls = spawn('ls', ['-lh', '/usr'])
+const { spawn } = require("child_process");
+const ls = spawn("ls", ["-lh", "/usr"]);
+
+//stdio
+ls.stdout.on("data", (data) => {
+  console.log(`stdout: ${data}`);
+});
+ls.stderr.on("data", (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+ls.on("close", (code) => {
+  console.log(`processo em segundo plano finalizado com sucesso ${code}`);
+});
