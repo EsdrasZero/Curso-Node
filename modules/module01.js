c2f792a8471e55b4b2cde32aa02693f4af729c9a
Esdras Mendes <esdrasmendes0@outlook.com>
2024-01-02 18:07:29 -0300

    Trabalhando com módulos no Node.

---
+++ modules/module01.js
@@ -0,0 +1,13 @@
+console.log("Executando o módulo modulo01.js...");
+
+const oculta = () => {
+  console.log("Função oculta...");
+};
+
+executa = () => {
+  console.log("Executando a função executa...");
+};
+
+welcome = "Bem-vindo ao módulo modulo01.js...";
+
+module.exports = { oculta, executa, welcome };
---
+++ modules/module02.js
@@ -0,0 +1,6 @@
+const {executa, oculta} = require("./module01.js")
+
+
+executa();
+
+oculta()
